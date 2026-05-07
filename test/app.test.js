51870452e02020bad946ef45697d5d73ad7b38fa
sonim1711 <sonimaurya0702@gmail.com>
2026-05-07 15:07:15 +0530

    Create app.test.js

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /', () => {
+  it('should return success message', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toBe('CI/CD PoC Application is running successfully on Feature App.');
+  });
+});
